cd17625e078918d37b094666c9a2f4b89b28033d
Ian Mwangi <ian.mwangi2@students.moringaschool.com>
2024-06-24 19:09:46 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+// saturday-fun
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+  // monday-work
+  const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  }
+
+  //wrap-adjective
+  function wrapAdjective(accumulative) {
+    return function(adjective) {
+        return `You are ${accumulative}${adjective}${accumulative}!`;
+    };
+  }
